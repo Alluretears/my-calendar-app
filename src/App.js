9329583f99722a772c14bd2491a80840f007de69
Zech.shi <zech.shi@cootek.cn>
2021-09-10 18:17:12 +0800

    custom event ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import "./App.scss";
-import React, { Fragment } from "react";
 import "./index.css";
+import "./App.scss";
+import React, { Fragment, useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 import moment from "moment";
 import "moment/locale/zh-cn";
-import Year from "./Year";
-import Toolbar from "./Toolbar";
 import clsx from "clsx";
 import * as dates from "date-arithmetic";
 import { Lunar } from "lunar-javascript";
+import Year from "./Year";
+import Toolbar from "./Toolbar";
 
 function App() {
+  // 设置为中文
   moment.locale("zh-cn", {
     week: {
+      // 从周日开始
       dow: 0,
     },
   });
@@ -21,6 +25,13 @@ function App() {
   const localizer = momentLocalizer(moment);
 
   let currentDate = new Date();
+
+  // 自定义时间间隔头部组件
+  const customTimeGutterHeader = () => (
+    <div className={"time-gutter-header"}>全天</div>
+  );
+
+  // 自定义周视图头部组件
   const customWeekHeader = ({ date, label, localizer }) => {
     const lunarDate = Lunar.fromDate(date);
     const isCurrent = dates.eq(date, currentDate, "day");
@@ -45,6 +56,8 @@ function App() {
       );
     }
   };
+
+  // 自定义月视图单个日期格子的头部组件，即显示日期和对应的农历
   const customDateHeader = ({ label, date, drilldownView, onDrillDown }) => {
     if (!drilldownView) {
       return <span>{label}</span>;
@@ -83,7 +96,6 @@ function App() {
         }
       }
     }
-
     return (
       <a href="#" onClick={onDrillDown} role="cell">
         <span className={"month-lunar-day"}>{lunarDate.getDayInChinese()}</span>
@@ -92,26 +104,44 @@ function App() {
     );
   };
 
-  const customTimeGutterHeader = () => (
-    <div className={"time-gutter-header"}>全天</div>
-  );
+  const customEventCellWrapper = (props) => {
+    return (
+      <Popup
+        trigger={(open) => props.children}
+        position={["right center", "bottom center", "top center"]}
+        on={"click"}
+        keepTooltipInside=".Calendar"
+        closeOnDocumentClick
+      >
+        <span>hello</span>
+      </Popup>
+    );
+  };
 
-  const customTimeSlotWrapper = (timeSlotWrapperProps) => {
-    return <div> {timeSlotWrapperProps.children}</div>;
+  const customMonthEvent = (props) => {
+    let { event, title } = props;
+    return (
+      <Fragment>
+        <span>{title}</span>
+        <span>{localizer.format(event.start, "H:mm")}</span>
+      </Fragment>
+    );
   };
 
   const components = {
     toolbar: Toolbar,
     timeGutterHeader: customTimeGutterHeader,
-    timeSlotWrapper: customTimeSlotWrapper,
+    eventWrapper: customEventCellWrapper,
     month: {
       dateHeader: customDateHeader,
+      event: customMonthEvent,
     },
     week: {
       header: customWeekHeader,
     },
   };
 
+  // 自定义单个日期格子容器的 style，包含周视图和月视图
   const customDayPropGetter = (date) => {
     if (date.getDay() === 0 || date.getDay() === 6)
       return {
@@ -129,19 +159,90 @@ function App() {
       };
   };
 
+  const localEventsStr = localStorage.getItem("events");
+  const localEvents = localEventsStr
+    ? JSON.parse(localEventsStr).map((e) =>
+        Object.assign(e, { start: new Date(e.start), end: new Date(e.end) })
+      )
+    : [];
+  const [events, setEvents] = useState(localEvents);
+
+  const handleSelect = ({ start, end, slots, action }) => {
+    if (action === "doubleClick") {
+      const current = new Date();
+      let event;
+      if (slots && slots.length === 1) {
+        // 当前选中的是月视图
+        if (dates.eq(slots[0], current, "day")) {
+          // 今天
+          const newStart = dates.add(current, 1, "hours");
+          newStart.setMinutes(0, 0, 0);
+          const newEnd = dates.add(newStart, 1, "hours");
+          event = {
+            start: newStart,
+            end: newEnd,
+            title: "新建日程",
+            isAllDay: true,
+          };
+        } else {
+          //非今天
+          const newStart = new Date(start);
+          newStart.setHours(9, 0, 0, 0);
+          const newEnd = dates.add(newStart, 1, "hours");
+          event = {
+            start: newStart,
+            end: newEnd,
+            title: "新建日程",
+            isAllDay: true,
+          };
+        }
+      } else {
+        const newEnd = dates.add(start, 1, "hours");
+        event = {
+          start: start,
+          end: newEnd,
+          title: "新建日程",
+          isAllDay: false,
+        };
+      }
+      if (event) {
+        setEvents([...events, event]);
+        localStorage.setItem("events", JSON.stringify([...events, event]));
+      }
+    }
+  };
+
   return (
     <div className="App">
       <Calendar
         className={"Calendar"}
         localizer={localizer}
-        events={[]}
-        toolbar={true}
-        components={components}
+        selectable={true}
         views={{
           day: true,
           week: true,
           month: true,
           year: Year,
+        }}
+        events={events}
+        onSelectSlot={handleSelect}
+        popup={true}
+        toolbar={true}
+        components={components}
+        step={15}
+        timeslots={4}
+        getDrilldownView={(
+          targetDate,
+          currentViewName,
+          configuredViewNames
+        ) => {
+          if (
+            currentViewName === "year" &&
+            configuredViewNames.includes("year")
+          ) {
+            return "month";
+          }
+          return "day";
         }}
         dayPropGetter={customDayPropGetter}
         formats={{
@@ -155,12 +256,26 @@ function App() {
           dateFormat: "D",
         }}
         messages={{
-          day: "日",
-          week: "周",
-          month: "月",
           year: "年",
+
+          date: "日期",
+          time: "时间",
+          event: "日程",
+          allDay: "全天",
+          week: "周",
+          work_week: "工作日",
+          day: "日",
+          month: "月",
+          previous: "前一个",
+          next: "后一个",
+          yesterday: "昨天",
+          tomorrow: "明天",
           today: "今天",
+          agenda: "议程",
+          noEventsInRange: "该日期范围内没有日程",
+          showMore: (total) => `还有${total}项...`,
         }}
+        dayLayoutAlgorithm={"no-overlap"}
       />
     </div>
   );
--- src/Year.js
+++ src/Year.js
@@ -1,96 +1,135 @@
-// import PropTypes from 'prop-types'
 import React from "react";
 import moment from "moment";
-
 import * as dates from "date-arithmetic";
 import { Navigate } from "react-big-calendar";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 import "./Year.scss";
 
-function createCalendar(currentDate) {
-  if (!currentDate) {
-    currentDate = moment();
-  } else {
-    currentDate = moment(currentDate);
+function DateCell(props) {
+  const { date, currentMonthDate, events, localizer, getDrilldownView } = props;
+  const today = dates.eq(date, new Date(), "day") ? "today" : "";
+
+  if (date.getMonth() < currentMonthDate.getMonth()) {
+    return (
+      <button disabled={true} className="date prev-month">
+        {date.getDate()}
+      </button>
+    );
   }
 
-  const first = currentDate.clone().startOf("month");
-  const last = currentDate.clone().endOf("month");
-  const weeksCount = Math.ceil((first.day() + last.date()) / 7);
-  const calendar = Object.assign([], { currentDate, first, last });
+  if (date.getMonth() > currentMonthDate.getMonth()) {
+    return (
+      <button disabled={true} className="date next-month">
+        {date.getDate()}
+      </button>
+    );
+  }
 
-  for (let weekNumber = 0; weekNumber < weeksCount; weekNumber++) {
-    const week = [];
-    calendar.push(week);
-    calendar.year = currentDate.year();
-    calendar.month = currentDate.month();
-
-    for (let day = 7 * weekNumber; day < 7 * (weekNumber + 1); day++) {
-      const date = currentDate.clone().set("date", day + 1 - first.day());
-      date.calendar = calendar;
-      week.push(date);
+  function renderEvents() {
+    if (events && events.length > 0) {
+      return (
+        <div>
+          {events.map((e) => (
+            <div className={"my-popup-content-wrapper"}>
+              <span>
+                <span className={"my-popup-content-dot"} />
+                <span className={"my-popup-content-title"}>{e.title}</span>
+              </span>
+              <span className={"my-popup-content-time"}>
+                {e.allDay ? "全天" : localizer.format(e.start, "HH:mm")}
+              </span>
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return <span className={"my-popup-content-empty"}>无日程</span>;
     }
   }
 
-  return calendar;
-}
+  let drilldownView = getDrilldownView(date);
 
-function CalendarDate(props) {
-  const { dateToRender, dateOfMonth } = props;
-  const today =
-    dateToRender.format("YYYY-MM-DD") === moment().format("YYYY-MM-DD")
-      ? "today"
-      : "";
-
-  if (dateToRender.month() < dateOfMonth.month()) {
-    return (
-      <button disabled={true} className="date prev-month">
-        {dateToRender.date()}
-      </button>
-    );
-  }
-
-  if (dateToRender.month() > dateOfMonth.month()) {
-    return (
-      <button disabled={true} className="date next-month">
-        {dateToRender.date()}
-      </button>
-    );
+  function handleDoubleClick(date, view, e) {
+    e.preventDefault();
+    props.onDrillDown(date, view);
   }
 
   return (
-    <button
-      className={`date in-month ${today}`}
-      onClick={() => props.onClick(dateToRender)}
+    <Popup
+      className="my-popup"
+      offsetX={8}
+      on={"click"}
+      trigger={
+        <button
+          className={`date in-month ${today}`}
+          onDoubleClick={(e) => handleDoubleClick(date, drilldownView, e)}
+        >
+          {date.getDate()}
+        </button>
+      }
+      position={"right center"}
+      closeOnDocumentClick
+      closeOnEscape
     >
-      {dateToRender.date()}
-    </button>
+      {renderEvents}
+    </Popup>
   );
 }
 
-class Calendar extends React.Component {
-  state = {
-    calendar: undefined,
-  };
+class MonthGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monthData: {
+        date: new Date(),
+        weeks: [],
+      },
+    };
+  }
+
+  getMonthData(date) {
+    const mom = date ? moment(date) : moment();
+
+    const first = mom.clone().startOf("month");
+    const last = mom.clone().endOf("month");
+    const weeksCount = Math.ceil((first.day() + last.date()) / 7);
+    const monthData = {
+      date: mom.toDate(),
+      weeks: [],
+    };
+
+    for (let weekNumber = 0; weekNumber < weeksCount; weekNumber++) {
+      const weekDates = [];
+      for (let day = 7 * weekNumber; day < 7 * (weekNumber + 1); day++) {
+        const date = mom.clone().set("date", day + 1 - first.day());
+        weekDates.push(date.toDate());
+      }
+      monthData.weeks.push(weekDates);
+    }
+
+    return monthData;
+  }
 
   componentDidMount() {
-    this.setState({ calendar: createCalendar(this.props.date) });
+    this.setState({ monthData: this.getMonthData(this.props.date) });
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.date !== prevProps.date) {
-      this.setState({ calendar: createCalendar(this.props.date) });
+      this.setState({ monthData: this.getMonthData(this.props.date) });
     }
   }
 
   render() {
-    if (!this.state.calendar) {
+    if (!this.state.monthData) {
       return null;
     }
-
+    let { events, localizer, ...props } = this.props;
     return (
       <div className="month">
         <div className="month-name">
-          {this.state.calendar.currentDate.format("MMMM").toUpperCase()}
+          {localizer.format(this.state.monthData.date, "MMMM").toUpperCase()}
         </div>
         <div className="day-row">
           {moment.weekdaysMin().map((day, index) => (
@@ -99,18 +138,28 @@ class Calendar extends React.Component {
             </span>
           ))}
         </div>
-        {this.state.calendar.map((week, index) => (
+        {this.state.monthData.weeks.map((week, index) => (
           <div key={index} className="date-row">
-            {week.map((date) => (
-              <CalendarDate
-                key={date.date()}
-                dateToRender={date}
-                dateOfMonth={this.state.calendar.currentDate}
-                onClick={(date) =>
-                  alert(`Will go to daily-view of ${date.format("YYYY-MM-DD")}`)
-                }
-              />
-            ))}
+            {week.map((date) => {
+              const dateStart = dates.startOf(date, "day");
+              const dateEnd = dates.endOf(date, "day");
+              const dateEvents = events.filter(
+                (e) =>
+                  dates.gte(dateEnd, e.end, "minutes") &&
+                  dates.lt(dateStart, e.start, "minutes")
+              );
+
+              return (
+                <DateCell
+                  {...props}
+                  key={date.getDate()}
+                  date={date}
+                  currentMonthDate={this.state.monthData.date}
+                  events={dateEvents}
+                  localizer={localizer}
+                />
+              );
+            })}
           </div>
         ))}
       </div>
@@ -119,15 +168,38 @@ class Calendar extends React.Component {
 }
 
 class Year extends React.Component {
+  inRange(e, start, end, accessors) {
+    let eStart = dates.startOf(accessors.start(e), "day");
+    let eEnd = accessors.end(e);
+
+    let startsBeforeEnd = dates.lte(eStart, end, "day");
+    // when the event is zero duration we need to handle a bit differently
+    let endsAfterStart = !dates.eq(eStart, eEnd, "minutes")
+      ? dates.gt(eEnd, start, "minutes")
+      : dates.gte(eEnd, start, "minutes");
+
+    return startsBeforeEnd && endsAfterStart;
+  }
+
   render() {
-    let { date, ...props } = this.props;
-    let range = Year.range(date);
+    let { date, events, accessors, ...props } = this.props;
     const months = [];
     const firstMonth = dates.startOf(date, "year");
-
     for (let i = 0; i < 12; i++) {
+      const m = new Date();
+      m.setMonth(i);
+      const monthStart = dates.startOf(m, "month");
+      const monthEnd = dates.endOf(m, "month");
+      const monthEvents = events.filter((e) =>
+        this.inRange(e, monthStart, monthEnd, accessors)
+      );
       months.push(
-        <Calendar key={i + 1} date={dates.add(firstMonth, i, "month")} />
+        <MonthGrid
+          {...props}
+          key={i + 1}
+          date={dates.add(firstMonth, i, "month")}
+          events={monthEvents}
+        />
       );
     }
 
